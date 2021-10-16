fd73f1f12feb8e25108cca22ce4a3da844cbc8a1
Joel <jpachecov89@gmail.com>
2021-10-15 21:32:36 -0500

    finished

---
+++ Oceans.Exercise/User.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Oceans.Exercise
+{
+    public class User
+    {
+        /// <summary>
+        /// The name of the user
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The user's chosen payment type
+        /// </summary>
+        public PaymentType PaymentType { get; set; }
+
+        /// <summary>
+        /// The user's subscriptions
+        /// </summary>
+        internal IEnumerable<Subscription> Subscriptions { get; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="User" /> class.
+        /// </summary>
+        /// <param name="subscriptions">The subscriptions the user has.</param>
+        public User(IEnumerable<Subscription> subscriptions)
+        {
+            this.Subscriptions = subscriptions;
+        }
+
+        /// <summary>
+        /// The number of expired subscriptions the user has
+        /// </summary>
+        public int ExpiredSubscriptions
+        {
+            get
+            {
+                //Use LINQ to get the right number
+                return Subscriptions.Where(x => x.ExpirationYear < DateTime.Now.Year || (x.ExpirationYear == DateTime.Now.Year && x.ExpirationMonth < DateTime.Now.Month)).Count();
+            }
+        }
+
+        /// <summary>
+        /// Rewrite this method using discard for output variable
+        /// </summary>
+        public void UpdateUserName()
+        {
+            const string strForExercise = "1";
+
+            if (int.TryParse(strForExercise, out int myOutputVariable))
+            {
+                Name = "Oceans Code Experts";
+            }
+            else
+            {
+                _ = myOutputVariable;
+            }
+        }
+
+        /// <summary>
+        /// Rewrite this method to return a tuple of Name, PaymentType and the local variable
+        /// </summary>
+        public Tuple<string, PaymentType, bool> UserInformation()
+        {
+            bool codeExperts = true;
+            return Tuple.Create(Name, PaymentType, codeExperts);
+        }
+
+        /// <summary>
+        /// Update the local variables by deconstructing the tuple from UserInformation
+        /// Follow the sugested logic to update the User Name
+        /// </summary>
+        public void UpdateUserInformation()
+        {
+            string nameFromTuple = string.Empty;
+            bool codeExpertsFromTuple = false;
+
+            //deconstruct the tuple
+            var user = UserInformation();
+
+            nameFromTuple = user.Item1;
+            codeExpertsFromTuple = user.Item3;
+
+            if (codeExpertsFromTuple)
+            {
+                Name = $"Recruiting {nameFromTuple}";
+            }
+            else
+            {
+                _ = nameFromTuple;
+            }
+        }
+    }
+}
---
+++ Oceans.Test/RecruitingTest.cs
@@ -0,0 +1,161 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using FluentAssertions;
+using Oceans.Exercise;
+
+namespace Oceans.Test
+{
+    public class RecruitingTest
+    {
+        [Fact]
+        public void SubscriptionsExpiredLastMonth()
+        {
+            var date = DateTime.Now.AddMonths(-1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            Assert.True(user.ExpiredSubscriptions == 2, $"Expected expired subscriptions: 2 - Your code returns: {user.ExpiredSubscriptions}");
+        }
+
+        [Fact]
+        public void SubscriptionsExpiringNextMonth()
+        {
+            var date = DateTime.Now.AddMonths(1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            Assert.True(user.ExpiredSubscriptions == 0, $"Expected expired subscriptions: 0 - Your code returns: {user.ExpiredSubscriptions}");
+        }
+
+        [Fact]
+        public void SubscriptionsExpiredLastYear()
+        {
+            var date = DateTime.Now.AddYears(-1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            Assert.True(user.ExpiredSubscriptions == 2, $"Expected expired subscriptions: 2 - Your code returns: {user.ExpiredSubscriptions}");
+        }
+
+        [Fact]
+        public void SubscriptionsExpiredLastYearPreviousMonth()
+        {
+            var date = DateTime.Now.AddYears(-1).AddMonths(-1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            Assert.True(user.ExpiredSubscriptions == 2, $"Expected expired subscriptions: 2 - Your code returns: {user.ExpiredSubscriptions}");
+        }
+
+        [Fact]
+        public void SubscriptionsExpiringNextYearNextMonth()
+        {
+            var date = DateTime.Now.AddYears(1).AddMonths(1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            Assert.True(user.ExpiredSubscriptions == 0, $"Expected expired subscriptions: 2 - Your code returns: {user.ExpiredSubscriptions}");
+        }
+
+        [Fact]
+        public void SubscriptionsExpiredLastMonth_UsingFluentAssertions()
+        {
+            var date = DateTime.Now.AddMonths(-1);
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            user.ExpiredSubscriptions.Should().Equals(2);
+        }
+
+        [Fact]
+        public void UserInformationUpdates()
+        {
+            var date = DateTime.Now;
+
+            var subscriptions = new List<Subscription>
+            {
+                new Subscription(date.Month, date.Year) { Name = "Netflix" },
+                new Subscription(date.Month, date.Year) { Name = "Amazon Prime" }
+            };
+
+            var user = new User(subscriptions)
+            {
+                Name = "Mike Mora",
+                PaymentType = PaymentType.AmericanExpress
+            };
+
+            user.UpdateUserName();
+
+            Assert.True(user.Name == "Oceans Code Experts", "User name updated successfully");
+
+            var userTuple = user.UserInformation();
+
+            Assert.True(userTuple.Item1 == "Oceans Code Experts", "User name in tuple management");
+
+            user.UpdateUserInformation();
+
+            Assert.True(user.Name == "Recruiting Oceans Code Experts", "User name updated successfully after tuple management");
+        }
+    }
+}
